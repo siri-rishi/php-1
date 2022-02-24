f6cab5df1e800fe504bd1aa660f1baf742bc0afb
Sirisha Vadagana <sirishasrinivas.v@gmail.com>
2022-02-24 19:08:37 +0530

    jenkinsfile added

--- docker-files/docker-script.sh
+++ docker-files/docker-script.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 sudo yum install docker -y
 sudo systemctl start docker
-sudo chmod -aG ec2-user docker
 sudo curl -L  "https://github.com/docker/compose/releases/download/1.21.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
